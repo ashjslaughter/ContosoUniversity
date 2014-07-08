5129891a5a938c7da31d4fca06055d563b33af47
Ashley Slaughter <ashley.j.slaughter@gmail.com>
2014-07-08 14:23:24 -0400

    Changes to sorting/searching students

--- ContosoUniversity/ContosoUniversity/Controllers/StudentsController.cs
+++ ContosoUniversity/ContosoUniversity/Controllers/StudentsController.cs
@@ -16,31 +16,36 @@ namespace ContosoUniversity.Controllers
         private SchoolContext db = new SchoolContext();
 
         // GET: Students
-       public ActionResult Index(string sortOrder) 
+       public ActionResult Index(string sortOrder, string searchString) 
        { 
            ViewBag.NameSortParm = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";    
            ViewBag.DateSortParm = sortOrder == "Date" ? "date_desc" : "Date";    
            var students = from s in db.Students                   
-                          select s;    
-           switch (sortOrder)    
-           {      
-               case "name_desc":          
-                   students = students.OrderByDescending(s => s.LastName);          
-                   break;       
-               case "Date":          
-                   students = students.OrderBy(s => s.EnrollmentDate);          
-                   break;       
-               case "date_desc":         
-                   students = students.OrderByDescending(s => s.EnrollmentDate);         
-                   break;      
-               default:          
-                   students = students.OrderBy(s => s.LastName);         
-                   break;   
-           }    
-           return View(students.ToList()); 
+                          select s;  
+           if (!String.IsNullOrEmpty(searchString))
+           {
+               students = students.Where(s => s.LastName.ToUpper().Contains(searchString.ToUpper())
+                    ||
+                s.FirstMidName.ToUpper().Contains(searchString.ToUpper()));
+           }
+           switch (sortOrder) 
+           { 
+               case "name_desc":            
+               students = students.OrderByDescending(s => s.LastName); 
+               break; 
+               case "Date":            
+                   students = students.OrderBy(s => s.EnrollmentDate); 
+                   break; 
+               case "date_desc":            
+                   students = students.OrderByDescending(s => s.EnrollmentDate); 
+                   break; 
+               default:             
+                   students = students.OrderBy(s => s.LastName); 
+                   break; 
+           }
+           return View(students.ToList());
        }
 
-        // GET: Students/Details/5
         public ActionResult Details(int? id)
         {
             if (id == null)
