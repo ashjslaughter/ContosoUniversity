1758a9b17309cff3b4105fd834cb76e754066094
Ashley Slaughter <ashley.j.slaughter@gmail.com>
2014-07-08 11:39:44 -0400

    Student Controller Delete
    Added try catch for error message in student controller get http method.

--- ContosoUniversity/ContosoUniversity/Controllers/StudentsController.cs
+++ ContosoUniversity/ContosoUniversity/Controllers/StudentsController.cs
@@ -105,19 +105,23 @@ namespace ContosoUniversity.Controllers
         }
 
         // GET: Students/Delete/5
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        public ActionResult Delete(int? id, bool? saveChangesError = false) 
+        { 
+            if (id == null) 
+            { 
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest); 
+            } 
+            if (saveChangesError.GetValueOrDefault()) 
+            { 
+                ViewBag.ErrorMessage = "Delete failed. Try again, and if the problem persists see your system administrator."; 
             }
-            Student student = db.Students.Find(id);
-            if (student == null)
-            {
-                return HttpNotFound();
-            }
-            return View(student);
-        }
+            Student student = db.Students.Find(id); 
+            if (student == null) 
+            { 
+                return HttpNotFound(); 
+            } 
+            return View(student); 
+        } 
 
         // POST: Students/Delete/5
         [HttpPost, ActionName("Delete")]
