a10e2190289e9179e931f41d28bd058951d91f3c
Ashley Slaughter <ashley.j.slaughter@gmail.com>
2014-07-08 11:35:37 -0400

    Student Controller
    Reformatted edit code

--- ContosoUniversity/ContosoUniversity/Controllers/StudentsController.cs
+++ ContosoUniversity/ContosoUniversity/Controllers/StudentsController.cs
@@ -95,7 +95,14 @@ namespace ContosoUniversity.Controllers
                     db.Entry(student).State = EntityState.Modified;         
                     db.SaveChanges();          
                     return RedirectToAction("Index");       
-                }    }    catch (DataException /* dex */)    {       //Log the error (uncomment dex variable name and add a line here to write a log.       ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists see your system administrator.");    }    return View(student); }
+                }    
+            }    
+            catch (DataException /* dex */)   
+            {   
+                //Log the error (uncomment dex variable name and add a line here to write a log.      
+                ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists see your system administrator.");    
+            }    return View(student); 
+        }
 
         // GET: Students/Delete/5
         public ActionResult Delete(int? id)
