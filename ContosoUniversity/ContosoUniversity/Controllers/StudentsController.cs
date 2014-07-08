c1c110a092ff8fb596bfedd66af45d191ecf7835
Ashley Slaughter <ashley.j.slaughter@gmail.com>
2014-07-08 11:33:40 -0400

    Student Controller
    Made changes to the edit fuctionality in the student controller

--- ContosoUniversity/ContosoUniversity/Controllers/StudentsController.cs
+++ ContosoUniversity/ContosoUniversity/Controllers/StudentsController.cs
@@ -85,17 +85,17 @@ namespace ContosoUniversity.Controllers
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "ID,LastName,FirstMidName,EnrollmentDate")] Student student)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(student).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            return View(student);
-        }
+        [ValidateAntiForgeryToken] 
+        public ActionResult Edit([Bind(Include = "ID, LastName, FirstMidName, EnrollmentDate")]Student student) 
+        {    
+            try    
+            {       
+                if (ModelState.IsValid)       
+                {         
+                    db.Entry(student).State = EntityState.Modified;         
+                    db.SaveChanges();          
+                    return RedirectToAction("Index");       
+                }    }    catch (DataException /* dex */)    {       //Log the error (uncomment dex variable name and add a line here to write a log.       ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists see your system administrator.");    }    return View(student); }
 
         // GET: Students/Delete/5
         public ActionResult Delete(int? id)
