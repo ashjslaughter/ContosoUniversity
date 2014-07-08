124bcc9ea7046c5efb7b983b8ad51e6ed09bff16
Ashley Slaughter <ashley.j.slaughter@gmail.com>
2014-07-08 12:56:44 -0400

    Revert "Delete"
    This reverts commit 5c7d627337e654c59a9dd92ba33610f27bd1e3a3.

--- ContosoUniversity/ContosoUniversity/Controllers/StudentsController.cs
+++ ContosoUniversity/ContosoUniversity/Controllers/StudentsController.cs
@@ -64,25 +64,25 @@ namespace ContosoUniversity.Controllers
         // POST: Students/Create
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
+        [HttpPost] 
+[ValidateAntiForgeryToken] 
         public ActionResult Create([Bind(Include = "LastName, FirstMidName, EnrollmentDate")]Student student)
-        {
-            try
-            {
-                if (ModelState.IsValid)
-                {
-                    db.Students.Add(student);
-                    db.SaveChanges();
-                    return RedirectToAction("Index");
-                }
-            }
-            catch (DataException /* dex */)
-            {
+        {     
+            try    
+            {        
+                if (ModelState.IsValid)        
+                {             
+                    db.Students.Add(student);             
+                    db.SaveChanges();             
+                    return RedirectToAction("Index");        
+                }     
+            }     
+            catch (DataException /* dex */)     
+            {        
                 //Log the error (uncomment dex variable name and add a line here to write a log.         
-                ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists see your system administrator.");
-            }
-            return View(student);
+                ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists see your system administrator.");     
+            }     
+            return View(student); 
         }
 
         // GET: Students/Edit/5
@@ -104,61 +104,62 @@ namespace ContosoUniversity.Controllers
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "ID, LastName, FirstMidName, EnrollmentDate")]Student student)
-        {
-            try
-            {
-                if (ModelState.IsValid)
-                {
-                    db.Entry(student).State = EntityState.Modified;
-                    db.SaveChanges();
-                    return RedirectToAction("Index");
-                }
-            }
-            catch (DataException /* dex */)
-            {
+        [ValidateAntiForgeryToken] 
+        public ActionResult Edit([Bind(Include = "ID, LastName, FirstMidName, EnrollmentDate")]Student student) 
+        {    
+            try    
+            {       
+                if (ModelState.IsValid)       
+                {         
+                    db.Entry(student).State = EntityState.Modified;         
+                    db.SaveChanges();          
+                    return RedirectToAction("Index");       
+                }    
+            }    
+            catch (DataException /* dex */)   
+            {   
                 //Log the error (uncomment dex variable name and add a line here to write a log.      
-                ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists see your system administrator.");
-            } return View(student);
+                ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists see your system administrator.");    
+            }    return View(student); 
         }
 
         // GET: Students/Delete/5
-        public ActionResult Delete(int? id, bool? saveChangesError = false)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        public ActionResult Delete(int? id, bool? saveChangesError = false) 
+        { 
+            if (id == null) 
+            { 
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest); 
+            } 
+            if (saveChangesError.GetValueOrDefault()) 
+            { 
+                ViewBag.ErrorMessage = "Delete failed. Try again, and if the problem persists see your system administrator."; 
             }
-            if (saveChangesError.GetValueOrDefault())
-            {
-                ViewBag.ErrorMessage = "Delete failed. Try again, and if the problem persists see your system administrator.";
-            }
-            Student student = db.Students.Find(id);
-            if (student == null)
-            {
-                return HttpNotFound();
-            }
-            return View(student);
-        }
+            Student student = db.Students.Find(id); 
+            if (student == null) 
+            { 
+                return HttpNotFound(); 
+            } 
+            return View(student); 
+        } 
 
         // POST: Students/Delete/5
-        [HttpPost]
+        [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id)
+        public ActionResult DeleteConfirmed(int id)
         {
-            try
-            {
-                Student student = db.Students.Find(id);
-                db.Students.Remove(student);
-                db.SaveChanges();
-            }
-            catch (DataException/* dex */)
-            {
-                //Log the error (uncomment dex variable name and add a line here to write a log.         
-                return RedirectToAction("Delete", new { id = id, saveChangesError = true });
-            }
+            Student student = db.Students.Find(id);
+            db.Students.Remove(student);
+            db.SaveChanges();
             return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
         }
     }
 }
