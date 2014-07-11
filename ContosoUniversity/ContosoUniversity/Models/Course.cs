ee1685683fca0b8dc1b70755d21d4f38c3e38ea7
Ashley Slaughter <ashley.j.slaughter@gmail.com>
2014-07-11 11:30:04 -0400

    JM test

---
+++ ContosoUniversity/ContosoUniversity/Models/Course.cs
@@ -0,0 +1,41 @@
+<<<<<<< HEAD
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+=======
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+>>>>>>> origin/master
+
+namespace ContosoUniversity.Models
+{
+    public class Course
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]         
+<<<<<<< HEAD
+        [Display(Name = "Number")]
+        public int CourseID { get; set; }  
+      
+        [StringLength(50, MinimumLength = 3)]
+        public string Title { get; set; }   
+      
+        [Range(0, 5)]
+        public int Credits { get; set; }
+
+        public int DepartmentID { get; set; }
+
+        public virtual Department Department (get; set;)
+        public virtual ICollection<Enrollment> Enrollments { get; set; } 
+        public virtual ICollection<Instructor> Instructors {get; set;}
+
+=======
+        public int CourseID { get; set; }        
+        public string Title { get; set; }         
+        public int Credits { get; set; }                  
+        public virtual ICollection<Enrollment> Enrollments { get; set; }    
+>>>>>>> origin/master
+    } 
+} 
---
+++ ContosoUniversity/ContosoUniversity/Models/Student.cs
@@ -0,0 +1,54 @@
+﻿using System; 
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema; 
+
+namespace ContosoUniversity.Models 
+{     
+    public class Student     
+    {         
+        public int ID { get; set; }
+<<<<<<< HEAD
+<<<<<<< HEAD
+        [Required] 
+        [StringLength(50, MinimumLength=1)]
+        [Display(Name = "Last Name")] 
+        public string LastName { get; set; }
+        [Required]
+        [StringLength(50, ErrorMessage = "First name cannot be longer than 50 characters.")]
+        [Display(Name = "First Name")] 
+        [Column("FirstName")]
+        public string FirstMidName { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Enrollment Date")]
+        public DateTime EnrollmentDate { get; set; }                 
+        
+
+        [Display(Name = "Full Name")]
+        public string FullName
+        { 
+            get 
+            { 
+                return LastName + ", " + FirstMidName;
+            } 
+        }
+        public virtual ICollection<Enrollment> Enrollments { get; set; }
+=======
+=======
+>>>>>>> origin/master
+        [StringLength(50)] 
+        public string LastName { get; set; }
+        [StringLength(50, ErrorMessage = "First name cannot be longer than 50 characters.")]
+        [Column("FirstName")]
+        public string FirstMidName { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)] 
+        public DateTime EnrollmentDate { get; set; }                 
+        public virtual ICollection<Enrollment> Enrollments { get; set; }     
+<<<<<<< HEAD
+>>>>>>> origin/master
+=======
+>>>>>>> origin/master
+    } 
+}
