3792b80f3fa45f92201264da511f9dcca933ebf6
Ashley Slaughter <ashley.j.slaughter@gmail.com>
2014-07-11 10:02:17 -0400

    Added attributes to datetime in studnet class

--- ContosoUniversity/ContosoUniversity/Models/Student.cs
+++ ContosoUniversity/ContosoUniversity/Models/Student.cs
@@ -8,7 +8,9 @@ namespace ContosoUniversity.Models
     {         
         public int ID { get; set; }         
         public string LastName { get; set; } 
-        public string FirstMidName { get; set; }        
+        public string FirstMidName { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)] 
         public DateTime EnrollmentDate { get; set; }                 
         public virtual ICollection<Enrollment> Enrollments { get; set; }     
     } 
